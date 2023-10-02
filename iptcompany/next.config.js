4cf5e876d8cb8dca42194c77d86e5621cc4ffe03
Tim Cain <cainstudios@gmail.com>
2023-10-02 08:26:39 -0500

    WIP gradiant examples

--- iptcompany/next.config.js
+++ iptcompany/next.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} 
- * i.pinimg.com
+ * i.pinimg.com i.postimg.cc
 */
 const nextConfig = {
     images: {
@@ -19,6 +19,12 @@ const nextConfig = {
             {
                 protocol: 'https',
                 hostname: 'i.pinimg.com',
+                port: '',
+                pathname: '/**/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'i.postimg.cc',
                 port: '',
                 pathname: '/**/**',
             },
--- iptcompany/src/app/dribbble/Glass_v2/page.tsx
+++ iptcompany/src/app/dribbble/Glass_v2/page.tsx
@@ -1,25 +1,104 @@
 "use client";
 import Image from "next/image";
-import { Fragment, useState } from "react";
+
+
 
 function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
 }
 
 export default function Example() {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
-
   return (
     <div
-      className="bg-cover grid grid-cols-3 gap-4 p-20"
       style={{
         backgroundImage:
           "url(https://www.kindacode.com/wp-content/uploads/2022/05/green.jpeg)",
       }}
+      className={` w-full min-h-screen flex flex-wrap justify-center items-center gap-3 py-5 `}
     >
-      <div className="w-72 h-72 bg-white bg-opacity-20 backdrop-blur-lg rounded drop-shadow-lg"></div>
-      <div className="w-72 h-72 bg-white bg-opacity-40 backdrop-blur-md rounded drop-shadow-lg"></div>
-      <div className="w-72 h-72 bg-white bg-opacity-50 backdrop-blur-xl rounded drop-shadow-lg"></div>
+      <div
+        style={{ backdropFilter: "blur(10px)" }}
+        className="w-10/12 md:w-1/4 bg-white bg-opacity-10 rounded p-3 text-white border border-gray-300 shadow-lg"
+      >
+        <div className="w-full mb-3 pb-3 border-b border-1 border-white">
+          <h3 className="text-xl font-semibold text-shadow">Something Good</h3>
+        </div>
+
+        <div>
+          <Image
+            src="https://i.postimg.cc/SxLx0fHV/bg01.jpg"
+            alt="image1"
+            className="w-full h-48 object-cover mb-2"
+            height={500}
+            width={500}
+          />
+          <p className="mb-3 tracking-wide text-base text-shadow">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa,
+            omnis.
+          </p>
+          <button className="backdrop bg-white bg-opacity-0 border border-white px-3 py-1.5 rounded focus:outline-none focus:ring-2 focus:ring-white focus:ring-opacity-40 hover:bg-opacity-10 text-lg">
+            Detail
+          </button>
+        </div>
+      </div>
+      <div
+        style={{ backdropFilter: "blur(10px)" }}
+        className={`w-10/12 md:w-1/4 bg-white bg-opacity-10 rounded p-3 text-white border border-white shadow-lg`}
+      >
+        {/* <!-- header --> */}
+        <div className="w-full mb-3 pb-3 border-b border-1 border-white">
+          <h3 className="text-xl font-semibold text-shadow">Something Good</h3>
+        </div>
+        {/* <!-- body --> */}
+        <div>
+          <Image
+            src="https://i.postimg.cc/J4khxLqf/bg02.jpg"
+            alt="image2"
+            className="w-full h-48 object-cover mb-2"
+            height={500}
+            width={500}
+          />
+          <p className="mb-3 tracking-wide text-base text-shadow">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa,
+            omnis.
+          </p>
+          <button
+            style={{ backdropFilter: "blur(10px)" }}
+            className={`bg-white bg-opacity-0 border border-white   px-3 py-1.5 rounded focus:outline-none focus:ring-2 focus:ring-white focus:ring-opacity-40 hover:bg-opacity-10 text-lg hover:bg-gradient-to-br from-red-500 to-orange-500 active:bg-gradient-to-br from-red-500 to-orange-500`}
+          >
+            Detail
+          </button>
+        </div>
+      </div>
+      <div
+        style={{ backdropFilter: "blur(10px)" }}
+        className={`w-10/12 md:w-1/4 bg-white bg-opacity-10 rounded p-3 text-white border border-white shadow-lg`}
+      >
+        {/* <!-- header --> */}
+        <div className="w-full mb-3 pb-3 border-b border-1 border-white">
+          <h3 className="text-xl font-semibold text-shadow">Something Good</h3>
+        </div>
+        {/* <!-- body --> */}
+        <div>
+          <Image
+            src="https://i.postimg.cc/VNYLzb8w/bg03.jpg"
+            alt="image3"
+            className="w-full h-48 object-cover mb-2"
+            height={500}
+            width={500}
+          />
+          <p className="mb-3 tracking-wide text-base text-shadow">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa,
+            omnis.
+          </p>
+          <button
+            style={{ backdropFilter: "blur(10px)" }}
+            className={`bg-white bg-opacity-0 border border-white   px-3 py-1.5 rounded focus:outline-none focus:ring-2 focus:ring-white focus:ring-opacity-40 hover:bg-opacity-10 text-lg hover:bg-gradient-to-br from-red-500 to-orange-500 active:bg-gradient-to-br from-red-500 to-orange-500`}
+          >
+            Detail
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
